dda02c83f28c6f0cb72982db36a7d6427a4b9a87
McVeah <blacksorcerers@hotmail.com>
2015-12-07 10:41:00 +0100

    SampleApp commit

---
+++ SampleApp/App.startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+
+    public static class Startup
+    {
+
+        public static void Main(string[] startupArguments)
+        {
+            Photon.Application.Start<App>(startupArguments);
+        }
+
+    }
+
+}
---
+++ SampleApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+
+    public class App
+        : Photon.Application
+    {
+
+
+
+    }
+
+}
